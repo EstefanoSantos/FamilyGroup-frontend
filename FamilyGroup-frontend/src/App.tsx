35c2b36c350120eaa0be18ffbdb4b9e5841cdfd6
Estefano Santos <estefanosantosbarros@gmail.com>
2025-02-12 14:35:45 -0300

    feat: app

--- FamilyGroup-frontend/src/App.tsx
+++ FamilyGroup-frontend/src/App.tsx
@@ -1,12 +1,17 @@
-import NewUserForm from './components/NewUserForm'
+import  AuthProvider from "./provider/AuthProvider";
+import  Routes  from "./routes/Routes";
+import HeaderComponent from "./components/HeadersComponent";
+import Footer from "./components/Footer";
 
 function App() {
-
+  
   return (
-    <>
-      <NewUserForm />
-    </>
-  )
+    <AuthProvider>  
+      <HeaderComponent />
+      <Routes/>
+      <Footer />
+    </AuthProvider>
+  )     
 }
 
 export default App

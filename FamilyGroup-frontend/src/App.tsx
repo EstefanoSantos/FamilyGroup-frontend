8bed4d8ba1f4ae92b7dbb9a517cf01f068aa2caa
Estefano Santos <estefanosantosbarros@gmail.com>
2025-02-13 15:51:00 -0300

    feat: use AuthProvider and routing

--- FamilyGroup-frontend/src/App.tsx
+++ FamilyGroup-frontend/src/App.tsx
@@ -1,15 +1,11 @@
 import  AuthProvider from "./provider/AuthProvider";
 import  Routes  from "./routes/Routes";
-import HeaderComponent from "./components/HeadersComponent";
-import Footer from "./components/Footer";
 
 function App() {
   
   return (
     <AuthProvider>  
-      <HeaderComponent />
       <Routes/>
-      <Footer />
     </AuthProvider>
   )     
 }
